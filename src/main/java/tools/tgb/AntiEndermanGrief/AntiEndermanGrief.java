f67abd7c22f57b6445cf9d3166257e376ddadeee
Jacob Bashista <tgb.bashista@gmail.com>
2021-01-30 18:49:06 -0500

    Tracker and Updater
    - Added Update Checker
    - Added bStats
    - Removed spaces in name

--- src/main/java/tools/tgb/AntiEndermanGrief/AntiEndermanGrief.java
+++ src/main/java/tools/tgb/AntiEndermanGrief/AntiEndermanGrief.java
@@ -1,22 +1,41 @@
 package tools.tgb.AntiEndermanGrief;
 
+import org.bstats.bukkit.Metrics;
 import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityChangeBlockEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.logging.Logger;
+
 public class AntiEndermanGrief extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
-        getLogger().info("Anti Enderman Grief Enabled!");
+
+
+        Logger logger = this.getLogger();
+
+        new UpdateChecker(this, 81788).getVersion(version -> {
+            if (!this.getDescription().getVersion().equalsIgnoreCase(version)) {
+                logger.info("§a----------------------------------------");
+                logger.info("§aNew Version of AntiEndermanGrief available");
+                logger.info("§a----------------------------------------");
+            }
+        });
+
+        int pluginId = 10183;
+        Metrics metrics = new Metrics(this, pluginId);
+
         getServer().getPluginManager().registerEvents(this, this);
+
+        getLogger().info("AntiEndermanGrief Enabled!");
     }
 
     @Override
     public void onDisable() {
-        getLogger().info("Anti Enderman Grief Disabled!");
+        getLogger().info("AntiEndermanGrief Disabled!");
     }
 
     @EventHandler
